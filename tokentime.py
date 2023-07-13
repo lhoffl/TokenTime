fb88637fa91ab1114678d9b4e45fd48d02ce1325
Matthew Hoffman <matt@lhoffl.com>
2023-07-12 20:38:41 -0400

    Update tokentime.py

--- tokentime.py
+++ tokentime.py
@@ -38,6 +38,23 @@ teamEmoji = {
     "Shanghai Dragons": "<:dragons:603620207482765493>"
 }
 
+def send(message):
+
+    webhookurl = "web"
+
+    formdata = "------:::BOUNDARY:::\r\nContent-Disposition: form-data; name=\"content\"\r\n\r\n" + message + "\r\n------:::BOUNDARY:::--"
+
+    connection = http.client.HTTPSConnection("discordapp.com")
+    connection.request("POST", webhookurl, formdata, {
+        'content-type': "multipart/form-data; boundary=----:::BOUNDARY:::",
+        'cache-control': "no-cache",
+    })
+
+    response = connection.getresponse()
+    result = response.read()
+
+    return result.decode("utf-8")
+
 syslog.syslog("Simon is looking for games")
 
 lockFile = '/home/fowl/tokenTime/token.lock'
@@ -55,6 +72,9 @@ html = response.content
 
 soup = BeautifulSoup(html, "lxml")
 data = json.loads(soup.select("[type='application/json']")[0].string)
+
+print(data)
+
 matches = data['props']['pageProps']['blocks'][2]['schedule']['tableData']['events'][0]['matches']
 matches += data['props']['pageProps']['blocks'][2]['schedule']['tableData']['events'][1]['matches']
 
@@ -68,8 +88,23 @@ for match in matches:
             team1 = match['competitors'][0]['name']
             team2 = match['competitors'][1]['name']
 
-            matchInfo =  teamEmoji[team1] + " " + team1 + " vs " + teamEmoji[team2] + " " + team2
+            team1Emoji = ""
+            if(teamEmoji.get(team1) != None):
+                team1Emoji = teamEmoji[team1]
+
+            team2Emoji = ""
+            if(teamEmoji.get(team2) != None):
+                team2Emoji = teamEmoji[team2]
+
+
+            matchInfo =  team1Emoji + " " + team1 + " vs " + team2Emoji + " " + team2
             gamesToday.append(matchInfo)
+
+
+print(gamesToday)
+
+if(len(gameToday) == 0):
+    quit()
 
 for match in matches:
 
@@ -85,14 +120,16 @@ for match in matches:
 
                 html = urllib.request.urlopen("https://www.youtube.com/@overwatchleague")
                 video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-                print("https://www.youtube.com/watch?v=" + video_ids[0])
+                video = ("https://www.youtube.com/watch?v=" + video_ids[0])
 
                 schedule = "\n".join(gamesToday)
-                webhook = DiscordWebhook(url='https://discord.com/api/webhooks/WEBHOOK', content= "Kick it Simon, it's Token Time! <@&779183041368555560>\n" + video_ids[0] + "\n" + "Today's schedule is:\n" + schedule +"\n")
+                webhook = DiscordWebhook(url='web', content= "Kick it Simon, it's Token Time! <@&779183041368555560>\n" + video + "\n" + "Today's schedule is:\n" + schedule +"\n")
                 response = webhook.execute()
                 syslog.syslog("Simon found the following games: " + schedule)
                 syslog.syslog("Simon found the following link: " + video_ids[0])
                 quit()
+                send("Kick it Simon, it's Token Time! <@&779183041368555560>\nhttps://overwatchleague.com/en-us/\n" + "Today's schedule is:\n" + schedule +"\n")
             elif not ENCORE_TODAY and match['isEncore']:
                 Path(encoreLockFile).touch()
                 ENCORE_TODAY = path.exists(encoreLockFile)
+               # send("Kick it Simon, it's Encore Time! <:outlawsBestTeam:603620338521211082> <@&784536328481406998> <:outlawsBestTeam:603620338521211082> https://overwatchleague.com/en-us/" + matchInfo + " is the first encore game")
